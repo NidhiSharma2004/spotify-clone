189c57f706174d28e3cd09b02e47dedb04cdbf1b
NidhiSharma2004 <vidhi6643@gmail.com>
2022-01-31 17:33:17 +0530

    files Added

---
+++ action.js
@@ -0,0 +1,57 @@
+
+
+
+let Player = document.getElementById("Player");
+
+const songItem = document.querySelectorAll(".songItem")
+
+let songs = [
+    {Name:"love you zindagi", filePath:"audio/song1.mp3", coverPath: "covers/download.jpg" },
+    {Name:"salm-e-Ishq", filePath:"audio/song2.mp3", coverPath: "covers/download.jpg" },
+    // {Name:"salm-e-Ishq", filePath:"audio/song3.mp3", coverPath: "covers/download.jpg" },
+    // {Name:"namo-namo", filePath:"audio/song4.mp3", coverPath: "covers/download.jpg" },
+    // {Name:"zara-zara", filePath:"audio/song5.mp3", coverPath: "covers/download.jpg" },
+    // {Name:"dilbar",       filePath:"audio/song6.mp3", coverPath: "covers/download.jpg" },
+    // {Name:"ishq sufiyana", filePath:"audio/song7.mp3", coverPath: "covers/download.jpg" },
+    // {Name:"vaaste",        filePath:"audio/song8.mp3", coverPath: "covers/download.jpg" },
+    // {Name:"o mere dil chain", filePath:"audio/song9.mp3", coverPath: "covers/download.jpg" },
+    // {Name:"salm-e-Ishq", filePath:"audio/song2.mp3", coverPath: "covers/download.jpg" },
+    // {Name:"salm-e-Ishq", filePath:"audio/song3.mp3", coverPath: "covers/download.jpg" },
+    // {Name:"namo-namo", filePath:"audio/song4.mp3", coverPath: "covers/download.jpg" },
+]
+
+songItem.forEach((element,i)=>{
+    element.querySelector(".songName").innerText = songs[i].Name;
+});
+
+
+
+  //CHANGE BTN
+const pauseIconClassName = 'fa-pause-circle';
+const playIconClassName = 'fa-play-circle';
+const btns = document.querySelectorAll('.btn')
+function onChange(event){
+    //  get the button elememt from the event
+    const buttonElement = event.currentTarget;
+
+    // check if play button class is present on our button
+    const isPlayButton = buttonElement.classList.contains(playIconClassName);
+
+    // if a play button, remove the play button class and add pause button class
+    if(isPlayButton){
+        buttonElement.classList.remove(playIconClassName);
+        buttonElement.classList.add(pauseIconClassName);
+        audioElement.play();
+    }else{
+        buttonElement.classList.add(playIconClassName);
+        buttonElement.classList.remove(pauseIconClassName);
+        audioElement.pause();
+    }
+    
+}
+btns.forEach(btn=>{
+    btn.addEventListener('click', onChange);
+    songUdate()
+})
+
+
