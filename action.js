1cf5418be227fe261d19d37fac6ddbef52b438bf
NidhiSharma2004 <vidhi6643@gmail.com>
2022-01-31 17:47:20 +0530

    song name added

--- action.js
+++ action.js
@@ -5,12 +5,14 @@ let Player = document.getElementById("Player");
 
 const songItem = document.querySelectorAll(".songItem")
 
+// to add song name dynamically 
+
 let songs = [
     {Name:"love you zindagi", filePath:"audio/song1.mp3", coverPath: "covers/download.jpg" },
     {Name:"salm-e-Ishq", filePath:"audio/song2.mp3", coverPath: "covers/download.jpg" },
-    // {Name:"salm-e-Ishq", filePath:"audio/song3.mp3", coverPath: "covers/download.jpg" },
-    // {Name:"namo-namo", filePath:"audio/song4.mp3", coverPath: "covers/download.jpg" },
-    // {Name:"zara-zara", filePath:"audio/song5.mp3", coverPath: "covers/download.jpg" },
+    {Name:"salm-e-Ishq", filePath:"audio/song3.mp3", coverPath: "covers/download.jpg" },
+    {Name:"namo-namo", filePath:"audio/song4.mp3", coverPath: "covers/download.jpg" },
+    {Name:"zara-zara", filePath:"audio/song5.mp3", coverPath: "covers/download.jpg" },
     // {Name:"dilbar",       filePath:"audio/song6.mp3", coverPath: "covers/download.jpg" },
     // {Name:"ishq sufiyana", filePath:"audio/song7.mp3", coverPath: "covers/download.jpg" },
     // {Name:"vaaste",        filePath:"audio/song8.mp3", coverPath: "covers/download.jpg" },
@@ -20,7 +22,9 @@ let songs = [
     // {Name:"namo-namo", filePath:"audio/song4.mp3", coverPath: "covers/download.jpg" },
 ]
 
+// after slecting all songitem i have to make each song ka index song ke index ke equall
 songItem.forEach((element,i)=>{
+    console.log(element,i)
     element.querySelector(".songName").innerText = songs[i].Name;
 });
 
