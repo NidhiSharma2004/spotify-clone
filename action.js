2705207ca4e0fe4dab9860e5cfaf0b599abf818d
NidhiSharma2004 <vidhi6643@gmail.com>
2022-02-01 11:07:58 +0530

    audio added

--- action.js
+++ action.js
@@ -1,77 +1,85 @@
-
-
-let audioElement = new Audio("audio/song1.mp3")
+let audioElement = new Audio("audio/song1.mp3");
 let Player = document.getElementById("Player");
-let icns = document.querySelectorAll(".icn")
-const songItem = document.querySelectorAll(".songItem")
+let icns = document.querySelectorAll(".icn");
+let songItem = document.querySelectorAll(".songItem");
 
-// to add song name dynamically 
+// to add song name dynamically
 
 let songs = [
-    {Name:"love you zindagi", filePath:"audio/song1.mp3", coverPath: "covers/download.jpg" },
-    {Name:"salm-e-Ishq", filePath:"audio/song2.mp3", coverPath: "covers/download.jpg" },
-    {Name:"salm-e-Ishq", filePath:"audio/song3.mp3", coverPath: "covers/download.jpg" },
-    {Name:"namo-namo", filePath:"audio/song4.mp3", coverPath: "covers/download.jpg" },
-    {Name:"zara-zara", filePath:"audio/song5.mp3", coverPath: "covers/download.jpg" },
-    // {Name:"dilbar",       filePath:"audio/song6.mp3", coverPath: "covers/download.jpg" },
-    // {Name:"ishq sufiyana", filePath:"audio/song7.mp3", coverPath: "covers/download.jpg" },
-    // {Name:"vaaste",        filePath:"audio/song8.mp3", coverPath: "covers/download.jpg" },
-    // {Name:"o mere dil chain", filePath:"audio/song9.mp3", coverPath: "covers/download.jpg" },
-    // {Name:"salm-e-Ishq", filePath:"audio/song2.mp3", coverPath: "covers/download.jpg" },
-    // {Name:"salm-e-Ishq", filePath:"audio/song3.mp3", coverPath: "covers/download.jpg" },
-    // {Name:"namo-namo", filePath:"audio/song4.mp3", coverPath: "covers/download.jpg" },
-]
-
+  {
+    Name: "love you zindagi",
+    filePath: "audio/song1.mp3",
+    coverPath: "covers/download.jpg",
+  },
+  {
+    Name: "salm-e-Ishq",
+    filePath: "audio/song2.mp3",
+    coverPath: "covers/download.jpg",
+  },
+  {
+    Name: "salm-e-Ishq",
+    filePath: "audio/song3.mp3",
+    coverPath: "covers/download.jpg",
+  },
+  {
+    Name: "namo-namo",
+    filePath: "audio/song4.mp3",
+    coverPath: "covers/download.jpg",
+  },
+  {
+    Name: "zara-zara",
+    filePath: "audio/song5.mp3",
+    coverPath: "covers/download.jpg",
+  },
+  // {Name:"dilbar",       filePath:"audio/song6.mp3", coverPath: "covers/download.jpg" },
+  // {Name:"ishq sufiyana", filePath:"audio/song7.mp3", coverPath: "covers/download.jpg" },
+  // {Name:"vaaste",        filePath:"audio/song8.mp3", coverPath: "covers/download.jpg" },
+  // {Name:"o mere dil chain", filePath:"audio/song9.mp3", coverPath: "covers/download.jpg" },
+  // {Name:"salm-e-Ishq", filePath:"audio/song2.mp3", coverPath: "covers/download.jpg" },
+  // {Name:"salm-e-Ishq", filePath:"audio/song3.mp3", coverPath: "covers/download.jpg" },
+  // {Name:"namo-namo", filePath:"audio/song4.mp3", coverPath: "covers/download.jpg" },
+];
 // after slecting all songitem i have to make each song ka index song ke index ke equall
-songItem.forEach((element,i)=>{
-    element.querySelector(".songName").innerText = songs[i].Name;
-    element.querySelectorAll(".songName").audioElement = songs[i].filePath;
+songItem.forEach((element, i) => {
+  element.querySelector(".songName").innerText = songs[i].Name;
 });
 
 // change icn on click
 // slect all the icns and use for each and add the class of pause and remove play class and vice-versa
-// now phele bar jo click hoga vo currentClick hoga classlist add or remove kr denge but jb second click hoga 
+// now phele bar jo click hoga vo currentClick hoga classlist add or remove kr denge but jb second click hoga
 // to vo currentClick bnega or phela currentClick last click bnega or hm uspe se remove krenge pause icn and add
-// kr denge play icn
-icns.forEach((icn)=>{
-   
-    icn.addEventListener("click",(e)=>{
-        let currentClick = e.target
-        if(currentClick.classList.contains("fa-play")){
-        currentClick.classList.remove("fa-play")
-        currentClick.classList.add("fa-pause")
-        audioElement.play()
-        }else{
-            currentClick.classList.add("fa-play")
-            currentClick.classList.remove("fa-pause")
-        }
-        icns.forEach((icn2)=>{
-            let lastClick = icn2.firstChild
-            if(lastClick!=currentClick){
-                console.log(lastClick)
-                lastClick.classList.remove("fa-pause")
-                lastClick.classList.add("fa-play")
-            }
-        })
-    })
-})
-
-// now adding audio
-
-
-
+// kr denge play icn 
+// audio element ke liye jis icn p click kiya hai uski id lenge or vhi same id pass kr denge audioelment.scr ke songs arry indx m 
+icns.forEach((icn) => {
+  icn.addEventListener("click", (e) => {
+      console.log(e.target.id)
+    let currentClick = e.target;
+    if (currentClick.classList.contains("fa-play")) {
+      currentClick.classList.remove("fa-play");
+      currentClick.classList.add("fa-pause");
+      audioElement.src = songs[e.target.id].filePath
+      audioElement.play()
+    } else {
+      currentClick.classList.add("fa-play");
+      currentClick.classList.remove("fa-pause");
+      audioElement.pause()
+    }
+    icns.forEach((icn2) => {
+      let lastClick = icn2.firstChild;
+      if (lastClick != currentClick) {
+        console.log(lastClick);
+        lastClick.classList.remove("fa-pause");
+        lastClick.classList.add("fa-play");
+      }
+    });
+  });
+});
 
 
 
 
 
-
-
-
-
-
-
-  //CHANGE BTN
+//CHANGE BTN
 // const pauseIconClassName = "fas fa-pause";
 // const playIconClassName = 'fa-play-circle';
 // const btns = document.querySelectorAll('.btn')
@@ -92,11 +100,9 @@ icns.forEach((icn)=>{
 //         buttonElement.classList.remove(pauseIconClassName);
 //         audioElement.pause();
 //     }
-    
+
 // }
 // btns.forEach(btn=>{
 //     btn.addEventListener('click', onChange);
 //     songUdate()
 // })
-
-
