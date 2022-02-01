23a54f7573b3bdd2e0f5ae6d13384bce023c6167
NidhiSharma2004 <vidhi6643@gmail.com>
2022-02-01 10:17:47 +0530

    added icns

--- action.js
+++ action.js
@@ -1,8 +1,8 @@
 
 
-
+let audioElement = new Audio("audio/song1.mp3")
 let Player = document.getElementById("Player");
-
+let icns = document.querySelectorAll(".icn")
 const songItem = document.querySelectorAll(".songItem")
 
 // to add song name dynamically 
@@ -24,38 +24,79 @@ let songs = [
 
 // after slecting all songitem i have to make each song ka index song ke index ke equall
 songItem.forEach((element,i)=>{
-    console.log(element,i)
     element.querySelector(".songName").innerText = songs[i].Name;
+    element.querySelectorAll(".songName").audioElement = songs[i].filePath;
 });
+
+// change icn on click
+// slect all the icns and use for each and add the class of pause and remove play class and vice-versa
+// now phele bar jo click hoga vo currentClick hoga classlist add or remove kr denge but jb second click hoga 
+// to vo currentClick bnega or phela currentClick last click bnega or hm uspe se remove krenge pause icn and add
+// kr denge play icn
+icns.forEach((icn)=>{
+   
+    icn.addEventListener("click",(e)=>{
+        let currentClick = e.target
+        if(currentClick.classList.contains("fa-play")){
+        currentClick.classList.remove("fa-play")
+        currentClick.classList.add("fa-pause")
+        audioElement.play()
+        }else{
+            currentClick.classList.add("fa-play")
+            currentClick.classList.remove("fa-pause")
+        }
+        icns.forEach((icn2)=>{
+            let lastClick = icn2.firstChild
+            if(lastClick!=currentClick){
+                console.log(lastClick)
+                lastClick.classList.remove("fa-pause")
+                lastClick.classList.add("fa-play")
+            }
+        })
+    })
+})
+
+// now adding audio
+
+
+
+
+
+
+
+
+
+
+
 
 
 
   //CHANGE BTN
-const pauseIconClassName = 'fa-pause-circle';
-const playIconClassName = 'fa-play-circle';
-const btns = document.querySelectorAll('.btn')
-function onChange(event){
-    //  get the button elememt from the event
-    const buttonElement = event.currentTarget;
+// const pauseIconClassName = "fas fa-pause";
+// const playIconClassName = 'fa-play-circle';
+// const btns = document.querySelectorAll('.btn')
+// function onChange(event){
+//     //  get the button elememt from the event
+//     const buttonElement = event.currentTarget;
 
-    // check if play button class is present on our button
-    const isPlayButton = buttonElement.classList.contains(playIconClassName);
+//     // check if play button class is present on our button
+//     const isPlayButton = buttonElement.classList.contains(playIconClassName);
 
-    // if a play button, remove the play button class and add pause button class
-    if(isPlayButton){
-        buttonElement.classList.remove(playIconClassName);
-        buttonElement.classList.add(pauseIconClassName);
-        audioElement.play();
-    }else{
-        buttonElement.classList.add(playIconClassName);
-        buttonElement.classList.remove(pauseIconClassName);
-        audioElement.pause();
-    }
+//     // if a play button, remove the play button class and add pause button class
+//     if(isPlayButton){
+//         buttonElement.classList.remove(playIconClassName);
+//         buttonElement.classList.add(pauseIconClassName);
+//         audioElement.play();
+//     }else{
+//         buttonElement.classList.add(playIconClassName);
+//         buttonElement.classList.remove(pauseIconClassName);
+//         audioElement.pause();
+//     }
     
-}
-btns.forEach(btn=>{
-    btn.addEventListener('click', onChange);
-    songUdate()
-})
+// }
+// btns.forEach(btn=>{
+//     btn.addEventListener('click', onChange);
+//     songUdate()
+// })
 
 
